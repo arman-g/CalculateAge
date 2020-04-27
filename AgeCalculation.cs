93d3c44e3dded51664a77580e8fbc3730d7c85df
Arman Ghazanchyan <arman-g@users.noreply.github.com>
2020-04-26 18:39:14 -0700

    Update AgeCalculation.cs
    New implementation that performs better and and calculates age accurately.

--- AgeCalculation.cs
+++ AgeCalculation.cs
@@ -16,41 +16,83 @@ public class Age
 
 public static class DateTimeExtensions
 {
-	/// <summary>
-	/// Calculate the age of a person based on the date of birth.
-	/// </summary>
-	/// <param name="dob">The date of birth of the person.</param>
-	/// <param name="presentDate">The present date. Defaults to <see cref="DateTime.Now"/>, if not specified.</param>
-	/// <remarks>This function supports leaper DOBs (Feb 29).</remarks>
-	/// <returns>An <see cref="Age"/> object containing years, months and days information.</returns>
-	public static Age CalculateAge(
-		this DateTime dob,
-		DateTime? presentDate = null)
-	{
-		presentDate ??= DateTime.Now;
-		var age = new Age
-		{
-			Years = (ushort)(presentDate.Value.Year - dob.Year)
-		};
+        /// <summary>
+        /// Indicates whether this date is in leap year.
+        /// </summary>
+        /// <param name="value">This <see cref="DateTime"/> instance.</param>
+        /// <returns>A boolean value indicating whether this date instance is in leap your.</returns>
+        public static bool IsInLeapYear(this DateTime value)
+        {
+            return DateTime.IsLeapYear(value.Year);
+        }
 
-		var decrement =
-			!(!DateTime.IsLeapYear(presentDate.Value.Year) &&
-			  DateTime.IsLeapYear(dob.Year) &&
-			  dob.DayOfYear == 60 &&
-			  presentDate.Value.DayOfYear == 59) &&
-			presentDate.Value.DayOfYear < dob.DayOfYear;
+        /// <summary>
+        /// Calculate the age of a person based on the date of birth.
+        /// </summary>
+        /// <param name="dob">The date of birth of the person.</param>
+        /// <param name="presentDate">The present date. Defaults to <see cref="DateTime.Now"/>, if not specified.</param>
+        /// <remarks>This function supports leaper DOBs (Feb 29).</remarks>
+        /// <returns>An <see cref="Age"/> object containing years, months and days information.</returns>
+        public static Age CalculateAge(
+            this DateTime dob,
+            DateTime? presentDate = null)
+        {
+            presentDate ??= DateTime.Now.ToPacificDate();
+            if (presentDate < dob) throw new ArgumentOutOfRangeException(
+                nameof(presentDate),
+                "Present date must be bigger than DOB.");
 
-		if (decrement) { age.Years -= 1; }
+            const byte maxMonths = 12;
+            const byte feb28 = 59;
 
-		var tempDt = dob.AddYears(age.Years);
-		if (decrement) { age.Months = 11; }
+            // indicates whether dob year has extra day compare to present date.
+            // Note, leap years have 366 days vs to 365 for regular years.
+            var extraDay = dob.IsInLeapYear() &&
+                           !presentDate.Value.IsInLeapYear() &&
+                           dob.DayOfYear > feb28 ? 1 : 0;
 
-		age.Months += (byte)(presentDate.Value.Month - tempDt.Month);
-		if (tempDt.AddMonths(age.Months) > presentDate.Value) { age.Months -= 1; }
+            // indicates whether dob has been celebrated in the present year.
+            var hasDobOccur = presentDate.Value.DayOfYear >= dob.DayOfYear - extraDay;
 
-		tempDt = tempDt.AddMonths(age.Months);
-		age.Days = (byte)presentDate.Value.Subtract(tempDt).Days;
-		
-		return age;
-	}
+            // calculate the years of age
+            var age = new Age
+            {
+                Years = (ushort)(presentDate.Value.Year - dob.Year - (hasDobOccur ? 0 : 1))
+            };
+
+            // calculate the months of age
+            if (hasDobOccur)
+            {
+                age.Months = (byte)(presentDate.Value.Month - dob.Month);
+                if (age.Months > 0 & presentDate.Value.Day < dob.Day)
+                {
+                    age.Months -= 1;
+                }
+            }
+            else
+            {
+                age.Months = (byte)(maxMonths - 1 - Math.Abs(presentDate.Value.Month - dob.Month));
+            }
+
+            // calculate the days of age
+            var presentMonth = dob.Month + age.Months;
+            if (presentMonth > maxMonths)
+            {
+                presentMonth = Math.Abs(dob.Month + age.Months - maxMonths);
+            }
+
+            if (presentMonth == presentDate.Value.Month)
+            {
+                age.Days = (byte)(presentDate.Value.Day - (dob.Day - extraDay));
+            }
+            else
+            {
+                age.Days = (byte)(
+                    DateTime.DaysInMonth(dob.Year + age.Years, presentMonth) -
+                    (dob.Day - (hasDobOccur ? extraDay : 0)) +
+                    presentDate.Value.Day);
+            }
+
+            return age;
+        }
 }
